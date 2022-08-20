fa3e7670e5bbe1757e0c0cd75c3b6eb4548e4add
AntonioIacono <antonio.iac98@outlook.it>
2022-08-20 11:17:04 +0200

    Initial Commit

---
+++ SAD-ClientApp/SAD_ClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAD_ClientAppApp.swift
+//  SAD-ClientApp
+//
+//  Created by Antonio Iacono on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SAD_ClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
